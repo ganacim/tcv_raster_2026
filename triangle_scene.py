d8a622fd356b1117524217f71577be7775e4a9f0
Francisco Ganacim <ganacim@impa.br>
2026-02-03 09:44:55 -0300

    Add Triangle Scene implementation with red triangle

---
+++ triangle_scene.py
@@ -0,0 +1,11 @@
+from base import BaseScene, Color
+from shapes import Triangle
+
+# class name should be Scene
+class Scene(BaseScene):
+    def __init__(self):
+        super().__init__("Triangle Scene")
+        self.background = Color(1, 1, 1)
+
+        # Add some triangles to the scene
+        self.add(Triangle((1.0, 1.0), (3.0, 1.0), (2.0, 3.0)), Color(1.0, 0.0, 0.0))  # Red triangle
